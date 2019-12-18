39427f49252ecc57122fb2f87a5ec96856b0a7aa
DrorUzi <dror.uzi5@gmail.com>
2019-12-18 13:09:44 +0200

    Updates

---
+++ precache-manifest.7b37aaa52fa815e5e1b270c77712f2f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716919720b74d3dd3fcb9a275aebf376",
+    "url": "/misterBitcoin-react/index.html"
+  },
+  {
+    "revision": "ed70f540bf517b6325c6",
+    "url": "/misterBitcoin-react/static/css/main.2b3ab7aa.chunk.css"
+  },
+  {
+    "revision": "ac43c836bab093204687",
+    "url": "/misterBitcoin-react/static/js/2.dbbf0ecf.chunk.js"
+  },
+  {
+    "revision": "86cf7f074fde330b07dbd8e5fbabf90d",
+    "url": "/misterBitcoin-react/static/js/2.dbbf0ecf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed70f540bf517b6325c6",
+    "url": "/misterBitcoin-react/static/js/main.b3de3a12.chunk.js"
+  },
+  {
+    "revision": "60b25310d393423376d0",
+    "url": "/misterBitcoin-react/static/js/runtime-main.64fbdfb9.js"
+  },
+  {
+    "revision": "d331535dd8a6d1dd8af1465bb2e92591",
+    "url": "/misterBitcoin-react/static/media/bitcoin.d331535d.png"
+  },
+  {
+    "revision": "bd8997ff37a1610c0c74a3ffdaabd778",
+    "url": "/misterBitcoin-react/static/media/coins.bd8997ff.png"
+  },
+  {
+    "revision": "9471c42e5c4d070cb3e2a7e06f0dbce3",
+    "url": "/misterBitcoin-react/static/media/hero1.9471c42e.jpg"
+  }
+]);
